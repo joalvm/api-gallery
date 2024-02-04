21287370fff82f5ae7a1a970265c88f7e914ca9d
Alejandro Vilchez <joalvm@gmail.com>
2024-02-04 18:53:55 -0500

    step 1: docker y hot-reload para el desarrollo

---
+++ 01 - docker and hot-reload/cmd/api/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, World 49!")
+	})
+
+	http.ListenAndServe(":3000", nil)
+}
---
+++ 01 - docker and hot-reload/go.mod
@@ -0,0 +1,3 @@
+module github.com/joalvm/api-gallery
+
+go 1.21.5
